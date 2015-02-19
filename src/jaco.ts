a30e0e84085ed4ae9a4af4836dca55dfd9d3ebf2
Yusuke Hirao <yusukehirao@me.com>
2015-02-20 01:24:51 +0900

    katakanaOnly関数追加

--- src/jaco.ts
+++ src/jaco.ts
@@ -174,4 +174,16 @@ module jaco {
 		return new Jaco(str).isOnlyHiragana();
 	}
 
+	/**
+	* カタカナだけで構成されているかどうか
+	*
+	* @version 1.1.0
+	* @since 1.1.0
+	* @param str 対象の文字列
+	* @return カタカナだけで構成されているかどうか
+	*/
+	export function katakanaOnly (str: string): boolean {
+		return new Jaco(str).isOnlyKatakana();
+	}
+
 }
