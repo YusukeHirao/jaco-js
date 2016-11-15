d7a51b987e39e80c093b0aeea0586a8595f9bdb7
Yusuke Hirao <yusukehirao@me.com>
2016-11-15 16:40:46 +0900

    関数のモジュール化

---
+++ src/fn/katakanaOnly.ts
@@ -0,0 +1,13 @@
+import Jaco from '../jaco';
+
+/**
+ * カタカナだけで構成されているかどうか
+ *
+ * @version 2.0.0
+ * @since 1.1.0
+ * @param str 対象の文字列
+ * @return カタカナだけで構成されているかどうか
+ */
+export default function (str: string): boolean {
+	return new Jaco(str).isOnlyKatakana();
+}
---
+++ src/fn/katakanize.ts
@@ -0,0 +1,14 @@
+import Jaco from '../jaco';
+
+/**
+ * カタカナ化
+ *
+ * @version 2.0.0
+ * @since 0.1.0
+ * @param str 対象の文字列
+ * @param isCombinate 濁点・半濁点を結合文字にするかどうか
+ * @return カタカナ化された文字列
+ */
+export default function (str: string, toWide: boolean = true): string {
+	return new Jaco(str).toKatakana(toWide).toString();
+}
