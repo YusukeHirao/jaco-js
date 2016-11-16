bc8a4c2c1c9c669c1c3af1ffc85cd02856555d80
Yusuke Hirao <yusukehirao@me.com>
2016-11-16 16:54:51 +0900

    _toArrayの追加

--- src/jaco.ts
+++ src/jaco.ts
@@ -948,4 +948,18 @@ export default class Jaco {
 		});
 		return this;
 	}
+
+	/**
+	 * 文字列を配列化する
+	 *
+	 * サロゲートペア文字列を考慮する
+	 *
+	 * @version 2.0.0
+	 * @since 2.0.0
+	 * @return 配列化された文字列
+	 */
+	private _toArray (): string[] {
+		return this.$.match(/[\uD800-\uDBFF][\uDC00-\uDFFF]|[^\uD800-\uDFFF]/g) || [];
+	}
+
 }
